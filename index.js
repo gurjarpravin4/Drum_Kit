195e2e132b8e9b51553877b7b2021e2b2d6ddd2f
gurjarpravin4 <89782460+gurjarpravin4@users.noreply.github.com>
2022-10-27 13:36:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+//finding out number of drum buttons
+var noOfDrumButtons = document.querySelectorAll(".drum").length;
+
+// Detecting Button Press
+for(var i=0; i<noOfDrumButtons; i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    var clickedButton = this.innerHTML;
+    makeSound(clickedButton);
+    buttonAnimation(clickedButton);
+  });
+}
+
+// Detecting Key Press
+document.addEventListener("keydown", function(event){
+  var pressedKey = event.key;
+  makeSound(pressedKey);
+  buttonAnimation(pressedKey);
+});
+
+// Function to check which key/ button was pressed
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "a":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "s":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "d":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "j":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "k":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "l":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    default:
+      console.log(key);
+    
+  }
+}
+
+// Function to generate button animations
+function buttonAnimation(key){
+  var activeButton = document.querySelector("." + key);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
